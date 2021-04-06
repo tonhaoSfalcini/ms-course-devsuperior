d8def40307592d1c4ead3eca60fffde18e20bc20
tonhaoSfalcini <antoniosfalcini@gmail.com>
2021-04-05 23:39:26 -0400

    Ribbon Client (again)

--- hr-worker/src/main/java/com/devsuperior/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package com.devsuperior.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.entities.Worker;
 import com.devsuperior.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	WorkerRepository workerRepository;
 	
@@ -28,6 +36,9 @@ public class WorkerResource {
 
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("PORT = "+env.getProperty("local.server.port"));
+
 		Worker w = workerRepository.findById(id).get();
 		
 		return ResponseEntity.ok(w);
